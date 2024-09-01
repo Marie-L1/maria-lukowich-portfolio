3f2b5d7df688b301ba1efd62cfed887c87d0d5c3
Maria Lukowich <mlukowich27@gmail.com>
2024-09-01 15:11:18 -0600

    styling header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from "../../assets/images/logo.png"
+import "./Header.scss"
 
 function Header() {
   return (
@@ -8,7 +9,7 @@ function Header() {
             <img className="header__logo" src={logo} alt="personal logo"></img>
             <h1 className="header__title">Maria Lukowich</h1>
         </div>
-        <nav>
+        <nav className="header__nav">
             <h2 className="header__nav-about" href="#"><a>About Me</a></h2>
             <h2 className="header__nav-projects" href="#"><a>Projects</a></h2>
         </nav>
