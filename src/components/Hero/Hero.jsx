63b3f28c3c9df218bc4fb4cbb8c9604b829db324
Maria Lukowich <mlukowich27@gmail.com>
2024-09-05 20:55:04 -0600

    adding hero elements and images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import "./Hero.scss";
+import myImg from "../../assets/images/Maria-Lukowich_LI (1).png";
+import linkedin from "../../assets/images/linkedin-logo.webp";
+import github from "../../assets/images/github-logo.png";
 
 function Hero() {
   return (
     <section className="hero">
-      <img className="hero__img" src="#" alt="#"></img>
-      <h2 className="hero__welcome"></h2>
+      <img className="hero__img" src={myImg} alt="#"></img>
+      <h2 className="hero__welcome">Welcome</h2>
       <div className="hero__contact-wrapper">
-        <h3>Contact Me:</h3>
         <a href="#">
-          <img className="hero__linkedin-img" src="#"></img>
+          <img className="hero__linkedin-img" src={linkedin}></img>
         </a>
         <a href="#">
-          <img className="hero__github-img"></img>
+          <img className="hero__github-img" src={github}></img>
         </a>
       </div>
     </section>
