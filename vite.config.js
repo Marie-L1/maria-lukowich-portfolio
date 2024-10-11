32e09d0f279b54a28074da2b7c90371a55deb468
Maria Lukowich <mlukowich27@gmail.com>
2024-10-10 20:51:52 -0600

    adding vercel file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/maria-lukowich-portfolio/', // This sets the correct base path
   plugins: [react()],
-});
+})
